3392a82151f4980cc71e8dcefd72c12f5b9c3854
Andrew Elliott <aelliott@issaonline.com>
2022-01-09 11:32:07 -0600

    add form validation

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,16 +1,66 @@
 import "../App.css";
+import {useState, useEffect} from 'react';
 
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState('');
+    const [confirmUser, setConfirmUser] = useState();
+
+    useEffect(() => {
+        fetch('http://localhost:8000/users')
+        .then ((res) => {
+            return res.json();
+        })
+        .then(res => {
+            setUser(res)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        console.log(user[0].email, user[0].password)
+        console.log(email, password)
+        if(email === user[0].email && password === user[0].password){
+            setConfirmUser(true)
+        } else {
+            setConfirmUser(false)
+        }
+    }
+
+    
+
+
     return ( 
         <div className="loginDiv">
-            <form className="loginForm">
-                <label for="email">Email:</label>
-                <input type="text" id="email" name="email" />
-                <label for="password">Password:</label>
-                <input type="text" id="password" name="password" />
-                <button>Submit</button>
+            <form className="loginForm" onSubmit={handleSubmit}>
+                <label>Email:</label>
+                <input 
+                    type="text" 
+                    id="email" 
+                    name="email"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                    className={!confirmUser ? "incorrect" : ""}
+                />
+                <label>Password:</label>
+                <input 
+                    type="text" 
+                    id="password" 
+                    name="password" 
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    className={!confirmUser ? "incorrect" : ""}
+                />
+                <button >
+                    Submit
+                </button>
             </form>
+            {!confirmUser ? <p>Incorrect Credentials</p> : <p>...Loading</p>}
         </div>
      );
 }
---
+++ src/Components/Todo.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const Todos = () => {
+
+    const [todos, setTodos] = useState('');
+
+    return ( 
+        <div>
+            {/* Input field for todos */}
+            {/* Display Todos */}
+            {/* Complete Todos */}
+            {/* Remove Todos */}
+            <input
+        </div>
+     );
+}
+ 
+export default Todos;
